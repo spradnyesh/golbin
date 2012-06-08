078bfe8fe9403b04698a0c2e5ba9ffe66eb64e2e
Pradnyesh Sawant <spradnyesh@gmail.com>
2012-06-08 14:37:24 +0900

    unsuccessfully tried adding daemon; will try sometime later

--- frontend/packages.lisp
+++ frontend/packages.lisp
@@ -1,18 +1,20 @@
 (restas:define-module :hawksbill.golbin.frontend
   (:use :cl :cl-who :local-time)
-  #|(:export :route-home-page
+  (:export :route-home
+		   :route-home-page
 		   :route-cat
 		   :route-cat-subcat
 		   :route-article
 		   :route-home
 		   :route-author
-		   :route-tag)|#)
+		   :route-tag))
 
 (in-package :hawksbill.golbin.frontend)
 
 (defvar *article-storage* nil)
 (defvar *article-pagination-limit* 10)
 
+(setf hunchentoot:*show-lisp-errors-p* t)
+(setf hunchentoot:*show-lisp-backtraces-p* t)
+#|(restas:stop-all)|#
 #|(restas:start :hawksbill.golbin.frontend :port 8000)|#
-#|(setf hunchentoot:*show-lisp-errors-p* t)|#
-#|(setf hunchentoot:*show-lisp-backtraces-p* t)|#
