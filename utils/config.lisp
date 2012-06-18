ab99454af6ced3c7ad6a7b577ebb60f49b5dd5dc
Pradnyesh Sawant <spradnyesh@gmail.com>
2012-06-18 13:44:26 +0900

    sfrd

--- utils/config.lisp
+++ utils/config.lisp
@@ -21,7 +21,27 @@
 (defvar *valid-envt* '("dev" "prod"))
 (defvar *valid-intl* '("IN"))
 (defvar *valid-lang* '("en-IN"))
-(defvar *config-tree* (sort (make-config-tree *dimensions*) #'(lambda (a b) (< (length a) (length b)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; config
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defvar *config-tree* nil "((('envt'))
+                            (('intl'))
+                            (('lang'))
+                            (('envt' 'intl'))
+                            (('envt' 'lang'))
+                            (('intl' 'lang'))
+                            (('intl' 'envt'))
+                            (('lang' 'intl'))
+                            (('lang' 'envt'))
+                            (('envt' 'intl' 'lang')
+                                    (('dev' 'IN' 'en-IN') ('name1' 'val1') ('name2' 'val2'))
+                                    (('prod' 'IN' 'en-IN') ('name1' 'val3') ('name2' 'val4')))
+                            (('envt' 'lang' 'intl'))
+                            (('intl' 'envt' 'lang'))
+                            (('intl' 'lang' 'envt'))
+                            (('lang' 'envt' 'intl'))
+                            (('lang' 'intl' 'envt')))")
 
 (defclass config ()
   ((name :initarg :name :initform nil :accessor name)
@@ -37,10 +57,14 @@
                      (list item)))
            (diff (set-difference list
                                  item)))
-      (push item rslt)
+      (push (list item) rslt)
       (dolist (d diff)
         (push (cons d item)
               queue)))))
+
+(setf *config-tree* (sort (make-config-tree *dimensions*)
+                          #'(lambda (a b)
+                              (< (length (first a)) (length (first b))))))
 
 #|(defmacro vcp (dimension list) ; dunno why this ain't working below
   `((eql dimension ,dimension)
@@ -61,26 +85,52 @@
           (push d rslt))))
     (nreverse rslt)))
 
-#|(conditionally-accumulate #'(lambda (c)
-                              (equal (third c) "lang"))
-                          (conditionally-accumulate #'(lambda (c)
-                                                        (equal (second c) "intl"))
-                                                    (conditionally-accumulate #'(lambda (c)
-                                                                                  (equal (first c) "envt"))
-                                                                              *config-tree*)))|#
+(defun get-dimension-node (full-dimensions-map &optional (current-dimensions-map full-dimensions-map) (degree 0) (root *config-tree*))
+  "traverse down to the specific dimension-name node
+   in the *same* order as given in the _dimensions_ parameter
+   pass 0: (GET-DIMENSION-NODE (('lang' 'en-US') ('envt' 'dev') ('intl' 'IN')))
+   pass 1: (GET-DIMENSION-NODE (('lang' 'en-US') ('envt' 'dev') ('intl' 'IN'))
+                           (('envt' 'dev') ('intl' 'IN')) 1
+                           (('lang') ('lang' 'intl') ('lang' 'envt')
+                            ('lang' 'envt' 'intl') ('lang' 'intl' 'envt')))
+   pass 2: (GET-DIMENSION-NODE (('lang' 'en-US') ('envt' 'dev') ('intl' 'IN'))
+                             (('intl' 'IN')) 2
+                             (('lang' 'envt') ('lang' 'envt' 'intl')))"
+  (let* ((current-dimension (first current-dimensions-map))
+         (rest-dimensions (rest current-dimensions-map))
+         (current-nodes (conditionally-accumulate #'(lambda (c)
+                                                      (equal (nth degree c)
+                                                             (first current-dimension)))
+                                                  root)))
+    (if (null rest-dimensions)
+        (conditionally-accumulate #'(lambda (c)
+                                      (= (length c)
+                                         (length full-dimensions-map)))
+                                  current-nodes)
+        (get-dimension-node full-dimensions-map rest-dimensions (1+ degree) current-nodes))))
+
+(defun get-dimension-value-node (full-dimensions-map current-dimensions-map nodes &optional (degree 0))
+  (let* ((current-dimension (first current-dimensions-map))
+         (rest-dimensions (rest current-dimensions-map))
+         (current-nodes (conditionally-accumulate #'(lambda (c)
+                                                      (equal (nth degree c)
+                                                             (first current-dimension)))
+                                                  root)))
+    (if (null rest-dimensions)
+        (conditionally-accumulate #'(lambda (c)
+                                      (= (length c)
+                                         (length full-dimensions-map)))
+                                  current-nodes)
+        (get-dimension-node full-dimensions-map rest-dimensions (1+ degree) current-nodes))))
+
 (defun add-config (name value dimensions)
   (let ((list nil)
         (dimension-name-node *config-tree*)
         (dimensions-map (get-dimensions-map dimensions)))
-    ;; traverse down to the specific dimension-name node
-    ;; in the *same* order as given in the _dimensions_ parameter
     (dotimes (d (length dimensions-map))
       (let* ((d+1 (1+ d))
              (dimension (nth d+1 dimensions-map)))
-        (setf dimension-name-node (conditionally-accumulate #'(lambda (c)
-                                                                (equal (nth d+1 c)
-                                                                       (first dimension)))
-                                                            dimension-name-node))))
+        (setf dimension-name-node )))
     (setf list dimension-name-node)
     ;; now inside the dimension-name node, traverse down to the dimension-value node
     (dotimes (d (length dimensions-map))
@@ -95,5 +145,5 @@
                   (list name value))
             dimension-name-node))))
 
-(defun get-config (name &key (envt nil) (intl nil) (lang nil))
-  (declare (ignore name envt intl lang)))
+(defun get-config (name dimensions)
+  (declare (ignore name dimensions)))
