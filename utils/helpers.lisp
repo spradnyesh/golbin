5f36818b2e7792a19a09b9dc89b4c937803d90fe
Pradnyesh Sawant <spradnyesh@gmail.com>
2012-06-20 17:13:25 +0900

    fix bug: this was a very tricky bug to find (utils/helpers should've depended on frontend/routes, but that wasn't possible), dunno how this was working till now

--- utils/helpers.lisp
+++ utils/helpers.lisp
@@ -39,16 +39,16 @@
     (if (> p max)
         max
         p)))
-(defun pagination-markup (route page-number max-results &optional (pagination-limit *pagination-limit*))
+(defmacro pagination-markup (route page-number max-results &optional (pagination-limit *pagination-limit*))
   "build URLs b/n route?p=low and route?p=high"
   ;; don't show pagination-markup when page-number = 13, *article-pagination-limit* = 10 and max-results = 100 ;)
-  (if (< (* page-number pagination-limit) max-results)
-      (with-html
-        (:ul
-         (loop for i
-            from (pagination-low page-number pagination-limit)
-            to (pagination-high page-number max-results pagination-limit) do
-            (if (eql page-number i)
-                (htm (:li :id "pagination-match" (str i)))
-                (htm (:li (:a :href (genurl route :page i) (str i))))))))
-      ""))
+  `(if (< (* ,page-number ,pagination-limit) ,max-results)
+       (with-html
+         (:ul :class "pagination"
+              (loop for i
+                 from (pagination-low ,page-number ,pagination-limit)
+                 to (pagination-high ,page-number ,max-results ,pagination-limit) do
+                 (if (eql ,page-number i)
+                     (htm (:li :class "pagination-match" (str i)))
+                     (htm (:li (:a :href (genurl ,route :page i) (str i))))))))
+       ""))
