eb260144ece11f45451f8f9150a0305191cef195
Pradnyesh Sawant <spradnyesh@gmail.com>
2012-07-13 14:16:51 +0900

    refactor: minor reorg; refactor: enabled access/error logging

--- utils/init.lisp
+++ utils/init.lisp
@@ -2,33 +2,3 @@
 
 (defgeneric model-init ()
   (:documentation "dummy function so that the below macro will work correctly"))
-
-(defmacro start/stop/restart-system (system)
-  `(progn
-     (defun ,(intern (string-upcase (format nil "~a-start" `,system))) ()
-       (unless *system-status*
-         (setf *system-status* t)
-         (init-config)
-         (model-init)
-         (db-connect))
-       (hu-init)
-       (start (get-config ,(format nil "~a.restas.package" `,system))
-              :port (get-config ,(format nil "~a.restas.port" `,system))))
-     (defun ,(intern (string-upcase (format nil "~a-stop" `,system))) ()
-       (restas-stop (get-config ,(format nil "~a.restas.port" `,system))))
-     (defun ,(intern (string-upcase (format nil "~a-restart" `,system))) ()
-       (,(intern (string-upcase (format nil "~a-start" `,system))))
-       (,(intern (string-upcase (format nil "~a-stop" `,system)))))))
-
-(defun restas-stop (port)
-  (dolist (acceptor restas::*acceptors*)
-      (when (= port (hunchentoot::acceptor-port acceptor))
-        (stop acceptor)
-        (setf restas::*acceptors* (remove acceptor restas::*acceptors*))))
-  (dolist (vhost restas::*vhosts*)
-      (when (= port (restas::vhost-port vhost))
-        (setf restas::*vhosts* (remove vhost restas::*vhosts*)))))
-
-(defun destroy-system ()
-  (stop-all)
-  (db-disconnect))
---
+++ utils/restas.lisp
@@ -0,0 +1,40 @@
+(in-package :hawksbill.utils)
+
+
+
+(defmacro start/stop/restart-system (system)
+  `(progn
+	 (ensure-directories-exist ,(format nil "/tmp/hunchentoot/~a/" system))
+	 (defclass ,(intern (string-upcase (format nil "~a-acceptor" `,system))) (restas-acceptor)
+	   ()
+	   (:default-initargs
+		:access-log-destination ,(format nil "/tmp/hunchentoot/~a/access_log" system)
+		 :message-log-destination ,(format nil "/tmp/hunchentoot/~a/error_log" system)))
+     (defun ,(intern (string-upcase (format nil "~a-start" `,system))) ()
+       (unless *system-status*
+         (setf *system-status* t)
+         (init-config)
+         (model-init)
+         (db-connect))
+       (hu-init)
+       (start (get-config ,(format nil "~a.restas.package" `,system))
+              :port (get-config ,(format nil "~a.restas.port" `,system))
+			  :acceptor-class ',(intern (string-upcase (format nil "~a-acceptor" `,system)))))
+     (defun ,(intern (string-upcase (format nil "~a-stop" `,system))) ()
+       (restas-stop (get-config ,(format nil "~a.restas.port" `,system))))
+     (defun ,(intern (string-upcase (format nil "~a-restart" `,system))) ()
+       (,(intern (string-upcase (format nil "~a-start" `,system))))
+       (,(intern (string-upcase (format nil "~a-stop" `,system)))))))
+
+(defun restas-stop (port)
+  (dolist (acceptor restas::*acceptors*)
+      (when (= port (hunchentoot::acceptor-port acceptor))
+        (stop acceptor)
+        (setf restas::*acceptors* (remove acceptor restas::*acceptors*))))
+  (dolist (vhost restas::*vhosts*)
+      (when (= port (restas::vhost-port vhost))
+        (setf restas::*vhosts* (remove vhost restas::*vhosts*)))))
+
+(defun destroy-system ()
+  (stop-all)
+  (db-disconnect))
