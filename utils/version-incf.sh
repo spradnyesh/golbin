2a0ea25cc7e1091e0bf966c9e5955bca1743c129
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-03-08 14:12:16 +0900

    fix bug: [util] version-increment not working

--- utils/version-incf.sh
+++ utils/version-incf.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-#set -x
+set -x
 
 cd ..
 
 ###################################################
 ## replace in template
 ###################################################
-rslt=`grep -r "$1-.*~a.$2" *`
+rslt=`find . -name template.lisp | xargs grep "$1-.*-min.$2"`
 file=`echo $rslt | cut -d ':' -f 1`
-oldVersion=`echo $rslt | cut -d ':' -f 2 | cut -d \" -f 2 | cut -d - -f 2 | cut -d \~ -f 1`
+oldVersion=`echo $rslt | cut -d ':' -f 3 | cut -d \" -f 2 | cut -d - -f 2`
 newVersion=`expr $oldVersion + 1`
-oldString=`echo $1-$oldVersion~a.$2`
-newString=`echo $1-$newVersion~a.$2`
+oldString=`echo $1-$oldVersion-min.$2`
+newString=`echo $1-$newVersion-min.$2`
 #echo [$file] [$oldString] [$oldVersion] [$newVersion] [$newString]
 sed -e "s/${oldString}/${newString}/" $file > $file.tmp && mv $file.tmp $file
 
 ###################################################
 ## git mv
 ###################################################
-file=`\find . -name "$1-$oldVersion.$2"`
+file=`\find . -name "$1-$oldVersion-min.$2"`
 dir=`dirname $file`
 cd $dir
 #git mv "$1-$oldVersion.$2" "$1-$newVersion.$2"
