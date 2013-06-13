a111ed39d955780e4eac8fbdb26176ad736cbda7
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-06-13 14:52:45 +0900

    fix bug: had made a typo in translation (because emacs shows it differently than browser) in one of the previous commits; fixed it

--- common/locale/mr-IN.lisp
+++ common/locale/mr-IN.lisp
@@ -1,5 +1,5 @@
 datetime=~a ला, ~a वाजता
-home=पिहलं-पान
+home=पहिलं-पान
 page-not-found=पान सापडलं नाही
 written-by-with-tags=~a ने ~a या िदवशी ~a वाजता ~a~a या श्रेणीत ~a हे tags वापरून िलिहले
 written-by-without-tags=~a ने ~a या िदवशी ~a वाजता ~a~a या श्रेणीत िलिहले
