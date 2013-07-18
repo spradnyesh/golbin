fce9d06e041fc494eb45ed9c5798107b95f181a8
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-07-18 18:24:55 +0900

    fix bug: [fe] translate content for mr-IN of cite in article

--- common/locale/mr-IN.lisp
+++ common/locale/mr-IN.lisp
@@ -1,6 +1,6 @@
 datetime=~a ला, ~a वाजता
 home=पहिलं-पान
 page-not-found=पान सापडलं नाही
-written-by-with-tags=~a ने ~a या िदवशी ~a वाजता ~a / ~a या श्रेणीत ~a हे tags वापरून िलिहले
-written-by-without-tags=~a ने ~a या िदवशी ~a वाजता ~a / ~a या श्रेणीत िलिहले
+written-by-with-tags=~a यांनीे ~a या दिवशी ~a वाजता ~a / ~a या श्रेणीत ~a हे टॆग्स वापरून लिहिले
+written-by-without-tags=~a यांनीे ~a या दिवशी ~a वाजता ~a / ~a या श्रेणीत लिहिले
 register-here=इथे नविन आहात? रजिस्टर करा!
