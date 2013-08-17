9305e265ce4085db5f0cd97f7a33db099ef69d36
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-08-18 07:40:42 +0900

    tkt: cron for saving db (comments/articles) on linode

--- utils/gc.sh
+++ utils/gc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# */10 * * * * /home/hawksbill/golbin/scripts/gc.sh 2>&1 >> /home/hawksbill/golbin/gc.log
+
 cd /home/hawksbill/golbin
-ga data/db/prod
+git add data/db/prod
 git commit -am "s db frd @ `date +%F--%T`"
--- utils/ping.sh
+++ utils/ping.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# */30 * * * * curl http://www.golb.in/ > /home/hawksbill/golbin/heartbeat/golb.in.html 2> /dev/null
+# * * * * * /home/hawksbill/golbin/scripts/ping.sh > /home/hawksbill/golbin/heartbeat/golb.in.article.html 2> /dev/null
+
 r=$RANDOM
 j=$(( r %= 1000 ))
 curl --connect-timeout 5 "www.golb.in/title-of-$j.html"
