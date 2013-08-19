d50fc928b7ea04d84ce565ce63f6709d73ab2b28
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-08-19 16:13:09 +0900

    refactor: remove extraneous if condition from ed/login

--- editorial/view/login.lisp
+++ editorial/view/login.lisp
@@ -4,31 +4,28 @@
   (template
    :title "Login"
    :js nil
-   :body (if (is-logged-in?)
-             (redirect (h-genurl 'r-home))
-             (fmtnil
-              (<:p :id "login-sub-hd"
-                   (translate "login-existing"))
-              (<:form :action (h-genurl 'r-login-post)
-                      :method "POST"
-                      :id "login"
-                      (<:fieldset :class "inputs"
-                                  (label-input "username" "text")
-                                  (label-input "password" "password")
-                                  (<:p (<:input :type "submit"
-                                                :name "submit"
-                                                :class "submit"
-                                                :value (translate "login"))))
-                      (let ((ed-lang (cookie-in "ed-lang")))
-                        (if ed-lang
-                            (translate "register-forgot"
-                                       (<:a :href (h-genurl 'r-why-register
-                                                            :lang ed-lang)
-                                            (translate "join-us"))
-                                       (<:a :href (h-genurl 'r-register-get
-                                                            :lang ed-lang)
-                                            (translate "forgot-password")))
-                            (redirect (h-genurl 'r-login-get :lang "en-IN")))))))))
+   :body (fmtnil (<:p :id "login-sub-hd"
+                      (translate "login-existing"))
+                 (<:form :action (h-genurl 'r-login-post)
+                         :method "POST"
+                         :id "login"
+                         (<:fieldset :class "inputs"
+                                     (label-input "username" "text")
+                                     (label-input "password" "password")
+                                     (<:p (<:input :type "submit"
+                                                   :name "submit"
+                                                   :class "submit"
+                                                   :value (translate "login"))))
+                         (let ((ed-lang (cookie-in "ed-lang")))
+                           (if ed-lang
+                               (translate "register-forgot"
+                                          (<:a :href (h-genurl 'r-why-register
+                                                               :lang ed-lang)
+                                               (translate "join-us"))
+                                          (<:a :href (h-genurl 'r-register-get
+                                                               :lang ed-lang)
+                                               (translate "forgot-password")))
+                               (redirect (h-genurl 'r-login-get :lang "en-IN"))))))))
 
 (defun v-login-post ()
   (let* ((username (post-parameter "username"))
