30b0b49dc1877e0cbef997cfc1a7b2f131f1adf7
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-08-21 11:17:04 +0900

    fix bug: [fe] url is not mandatory in comments

--- frontend/view/comments.lisp
+++ frontend/view/comments.lisp
@@ -8,13 +8,10 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; helpers
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defun validate-comment (name email url challenge response userip)
+(defun validate-comment (name email challenge response userip)
   (let ((err0r nil))
     (cannot-be-empty name "name" err0r)
-    (when (and (nil-or-empty email)
-               (nil-or-empty url))
-      (push (translate "one-of-email-or-url-should-be-given")
-           err0r))
+    (cannot-be-empty email "email" err0r)
     (unless (validate-email email)
       (push (translate "invalid-email") err0r))
     (multiple-value-bind (status error-code)
@@ -45,7 +42,9 @@
                                :action (h-genurl 'r-article-comment :id article-id)
                                (<:table
                                 (tr-td-input "name" :mandatory t)
-                                (tr-td-input "email" :tooltip "mandatory, but will not be shown")
+                                (tr-td-input "email"
+                                             :tooltip "mandatory, but will not be shown"
+                                             :mandatory t)
                                 (tr-td-input "url")
                                 (tr-td-text "comment" :mandatory t)
                                 (tr-td-input "challenge" :class "hidden" :id t :value "")
@@ -70,7 +69,7 @@
         (challenge (post-parameter "challenge"))
         (response (post-parameter "response"))
         (userip (remote-addr*)))
-    (let ((err0r (validate-comment name email url challenge response userip)))
+    (let ((err0r (validate-comment name email challenge response userip)))
       (if (not err0r)
           (progn
             (add-comment (make-instance 'comment
