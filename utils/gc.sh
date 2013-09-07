ee725c56361c8e5e2c08607fec50a13debb15b6c
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-09-08 01:24:35 +0900

    tkt: cron for saving db (comments/articles) on linode

--- utils/gc.sh
+++ utils/gc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# */10 * * * * $HOME/golbin/scripts/gc.sh 2>&1 >> $HOME/golbin/logs/gc.log
+# */10 * * * * $HOME/golbin/utils/gc.sh 2>&1 >> $HOME/golbin/logs/gc.log
 
 cd $HOME/golbin
 git add data/db/prod
 git commit -am "s db frd @ `date +%F--%T`"
+git push
