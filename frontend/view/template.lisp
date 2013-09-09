6b648608cac2f6819adcc9f4e7dc9ec574c02b4f
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-09-09 13:44:03 +0900

    fix: [bug][utils] loading icon not working in chrome

--- frontend/view/template.lisp
+++ frontend/view/template.lisp
@@ -29,7 +29,7 @@
                                   :href "/static/css/yui3-reset-fonts-grids-min.css")
                             (<:style (get-css)))
                           (<:link :rel "stylesheet" :type "text/css"
-                                  :href "/static/css/fe-24-min.css")
+                                  :href "/static/css/fe-25-min.css")
                           ;; google analytics and adsense
                           (<:script :type "text/javascript" "
   var _gaq = _gaq || [];
