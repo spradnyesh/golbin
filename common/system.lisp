414f6e88e5cfd81ebf62268f7a81428aaba7a0bc
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-11-24 15:48:28 +0900

    fix: [fed] improve golbin-restart

--- common/system.lisp
+++ common/system.lisp
@@ -1,5 +1,7 @@
 (in-package :hawksbill.golbin)
 
 (defun golbin-restart ()
+  (ql:quickload :golbin)
+  (setf *system-status* nil)
   (hawksbill.golbin.frontend::fe-restart)
   (hawksbill.golbin.editorial::ed-restart))
