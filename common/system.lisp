a54a5969719df5ca2bfab7793ce4871e73fafbb1
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-12-01 10:54:52 +0900

    refactor: made quickload optional in golbin-restart

--- common/system.lisp
+++ common/system.lisp
@@ -1,7 +1,8 @@
 (in-package :hawksbill.golbin)
 
-(defun golbin-restart ()
-  (ql:quickload :golbin)
+(defun golbin-restart (&optional (restart nil))
+  (when restart
+    (ql:quickload :golbin))
   (setf *system-status* nil)
   (hawksbill.golbin.frontend::fe-restart)
   (hawksbill.golbin.editorial::ed-restart))
