cd96c392b70fca9b02dddfb76e5eb65204bb2ec9
Pradnyesh Sawant <spradnyesh@gmail.com>
2013-12-14 01:09:10 +0900

    fix: corrected db commit cron script

--- utils/gc.sh
+++ utils/gc.sh
@@ -2,7 +2,7 @@
 
 # */10 * * * * $HOME/golbin/utils/gc.sh 2>&1 >> $HOME/golbin/logs/gc.log
 
-cd $HOME/golbin
-git add data/db/prod
+cd $HOME/golbin/data/db
+git add prod
 git commit -am "s db frd @ `date +%F--%T`"
 git push
