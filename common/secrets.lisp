c4ad32727444314f700f640185d97824f5428501
Pradnyesh Sawant <spradnyesh@gmail.com>
2014-01-24 13:34:01 +0900

    refactor: populate *secrets* too, so that next time its value will be picked automatically

--- common/secrets.lisp
+++ common/secrets.lisp
@@ -1,13 +1,16 @@
 (in-package :hawksbill.golbin)
 
 (defmacro populate-config-from-secret (key)
-  `(progn
-     (if (assoc ,key *secrets* :test #'equal)
-         (add-config ,key
-                     (cdr (assoc ,key *secrets* :test #'equal))
-                     "master")
-         (progn (format t "please enter ~a key: " ,key)
-                (add-config ,key (read-line) "master")))))
+  (let ((value (gensym)))
+    `(progn
+       (if (assoc ,key *secrets* :test #'equal)
+           (add-config ,key
+                       (cdr (assoc ,key *secrets* :test #'equal))
+                       "master")
+           (progn (format t "please enter ~a key: " ,key)
+                  (setf ,value (read-line))
+                  (push-to-secret ,key ,value)
+                  (add-config ,key ,value "master"))))))
 
 (defun push-to-secret (key value)
   (push (cons key value) *secrets*))
